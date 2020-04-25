11eb9ed61d547e052da53a26499f52fcea1d668c
dhruvik1999 <navadiyadhruvik2@gmail.com>
2020-04-25 20:50:44 +0530

    tinker added

--- src/index.py
+++ src/index.py
@@ -1,14 +1,26 @@
 from tkinter import *
+from tkinter import filedialog
+import os
+import analyser
 
 window = Tk()
 window.title("Protocol Analyzer")
 window.geometry('1100x1000')
 
-lbl = Label(window,text="Wellcome to Protocol analyzer",  font=("Arial Bold", 50))
-lbl.grid(column=0,row=0)
+lbl = Label(window,text="Protocol Analyzer",  font=("Arial Bold", 50))
+lbl.grid(column=10,row=0)
 
-btn = Button(window, text="Click Me")
-btn.grid(column=1, row=5)
+def search_for_file_path ():
+    currdir = os.getcwd()
+    tempdir = filedialog.askopenfilename(parent=window, initialdir=currdir, title='Please select a directory')
+    if len(tempdir) > 0:
+        print ("You chose: %s" % tempdir)
+        return tempdir
+    return None
+
+file_path_variable = search_for_file_path()
+frames = analyser.readFile(file_path_variable)
+analyser.disp_prot_details(frames)
 
 
 
