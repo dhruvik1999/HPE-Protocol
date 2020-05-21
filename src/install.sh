38c759aae347a2ff14fc682dad4100550636a153
dhruvik1999 <navadiyadhruvik2@gmail.com>
2020-05-21 09:56:56 +0530

    install lib added

---
+++ src/install.sh
@@ -0,0 +1,13 @@
+echo "System updating..."
+sudo apt-get update
+echo "Installing python 3.6 ..."
+sudo apt-get install python3.6
+echo "Installing python 3 tkinter ..."
+sudo apt-get install python3-tk
+echo "Installing pip3 ..."
+sudo apt install python3-pip
+echo "Installing matplotlib ..."
+sudo pip3 install matplotlib
+echo "install scapy"
+pip install scapy
+echo "Done :)"
